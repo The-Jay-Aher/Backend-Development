c5d2121b58514fdd6f3b301e84fc0fb4e64901b9
Jay Aher <jaysureshaher@gmail.com>
2023-11-15 15:11:20 +0530

    Second Commit

--- Chai aur Code/index.js
+++ Chai aur Code/index.js
@@ -4,10 +4,49 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+const githubData = {
+	login: 'The-Jay-Aher',
+	id: 115747060,
+	node_id: 'U_kgDOBuYo9A',
+	avatar_url: 'https://avatars.githubusercontent.com/u/115747060?v=4',
+	gravatar_id: '',
+	url: 'https://api.github.com/users/The-Jay-Aher',
+	html_url: 'https://github.com/The-Jay-Aher',
+	followers_url: 'https://api.github.com/users/The-Jay-Aher/followers',
+	following_url: 'https://api.github.com/users/The-Jay-Aher/following{/other_user}',
+	gists_url: 'https://api.github.com/users/The-Jay-Aher/gists{/gist_id}',
+	starred_url: 'https://api.github.com/users/The-Jay-Aher/starred{/owner}{/repo}',
+	subscriptions_url: 'https://api.github.com/users/The-Jay-Aher/subscriptions',
+	organizations_url: 'https://api.github.com/users/The-Jay-Aher/orgs',
+	repos_url: 'https://api.github.com/users/The-Jay-Aher/repos',
+	events_url: 'https://api.github.com/users/The-Jay-Aher/events{/privacy}',
+	received_events_url: 'https://api.github.com/users/The-Jay-Aher/received_events',
+	type: 'User',
+	site_admin: false,
+	name: 'Jay Aher',
+	company: null,
+	blog: '',
+	location: 'Thane',
+	email: null,
+	hireable: true,
+	bio: null,
+	twitter_username: null,
+	public_repos: 21,
+	public_gists: 0,
+	followers: 0,
+	following: 1,
+	created_at: '2022-10-13T22:24:32Z',
+	updated_at: '2023-11-14T14:47:39Z',
+};
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/twitter', (req, res) => {
 	res.send('jayaherdotcom');
+});
+
+app.get('/github', (req, res) => {
+	res.json(githubData);
 });
 
 app.get('/login', (req, res) => {
